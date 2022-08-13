bc210e3d579ec114baf4ad12ba30f3de3c681cd3
nely de faria <nelly.nf54@gmail.com>
2022-08-13 20:12:28 -0300

    Cria index.js

---
+++ js-poo-2/index.js
@@ -0,0 +1,14 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+
+const cliente1 = new Cliente('Ricardo', 64062389070);
+
+const contaCorrente = new ContaCorrente(cliente1, 1001);
+contaCorrente.depositar(500);
+contaCorrente.sacar(100);
+
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
+console.log(contaCorrente);
+console.log(contaPoupanca);
