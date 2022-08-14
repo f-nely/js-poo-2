2749fd54df73de9546320fc3739fb8649eb9a335
nely de faria <nelly.nf54@gmail.com>
2022-08-14 19:41:16 -0300

    Instanciando classe ContaSalario

--- js-poo-2/index.js
+++ js-poo-2/index.js
@@ -1,15 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente('Ricardo', 64062389070);
 
 const contaCorrente = new ContaCorrente(cliente1, 1001);
-contaCorrente.depositar(500);
-contaCorrente.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
 
-console.log(contaCorrente);
-console.log(contaPoupanca);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
